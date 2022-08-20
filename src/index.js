a8100523d1c1970c543c9f6d05513794393aae1f
Kateryna Andriichuk <hkatrina007@gmail.com>
2022-08-20 23:11:40 +0300

    updating according to last homework
    JS: current location button, search engine, current data displaying.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+//--API--//
+
+function getDataTemp(response) {
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#res").innerHTML = Math.round(
+        response.data.main.temp
+    );
+    document.querySelector("#hum").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = response.data.wind.speed;
+    console.log(response);
+}
+
+getData("Kyiv");
+
+function getData(city) {
+    let apiKey = "f2a7bf87b777299f2d3968c89592e347";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiURL).then(getDataTemp);
+}
+
+function submit(event) {
+    event.preventDefault();
+    let cName = document.querySelector("#formGroupExampleInput").value;
+    getData(cName);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", submit);
+
+//--CURRENT LOCATION BUTTON--//
+
+function showTempPos(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let apiKey = `f2a7bf87b777299f2d3968c89592e347`;
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiURL).then(getDataTemp);
+}
+
+function getCurrentPosition(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(showTempPos);
+}
+
+let locButton = document.querySelector("#location");
+locButton.addEventListener("click", getCurrentPosition);
+
+//--DATA AND TIME--//
+
 let now = new Date();
 console.log(now.getHours());
 console.log(now.getMinutes());
@@ -22,7 +72,6 @@ projDay.innerHTML = todDay;
 
 let todMin = now.getMinutes();
 let todHours = now.getHours();
-
 if (todHours < 10) {
     todHours = `0${todHours}`;
 }
@@ -30,7 +79,6 @@ if (todHours < 10) {
 if (todMin < 10) {
     todMin = `0${todMin}`;
 }
-
 let projTime = document.querySelector("#currTime");
 projTime.innerHTML = `${todHours}:${todMin}`;
 console.log(projTime);
@@ -44,52 +92,23 @@ let currYear = now.getFullYear();
 let projDate = document.querySelector("#currDate");
 projDate.innerHTML = `${todMonth}/${currDay}/${currYear}`;
 
+//--TEMPERATURE--//
 
+let celtemp = document.querySelector("#cel");
+let fahrtemp = document.querySelector("#fahr");
+let restemp = document.querySelector("#res");
 
+console.log(celtemp);
+console.log(fahrtemp);
+console.log(restemp);
 
-
-
-
-
-
-
-
-
-function showInfo(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#res").innerHTML = Math.round(response.data.main.temp);
-    document.querySelector("#hum").innerHTML = `Humidity: ${Math.round(
-        response.data.main.humidity
-    )}%`;
-    document.querySelector("#wind").innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
-
-
-    let currTemp = Math.round(response.data.main.temp);
-
-    let place = document.querySelector("#res");
-    place.innerHTML = currTemp;
-    axios.get(apiURL).then(getInfo);
-    console.log(response.data);
+function showcel(event) {
+    restemp.innerHTML = 20;
 }
 
-function showDefaultInfo(city) {
-
-    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=${city},au&appid=${apiKey}&units=metric";
-    let apiKey = "7e7a6afccc2b04cbebfc65ac42faa6dc";
-    axios.get(apiURL).then(showInfo);
+function showfahr(event) {
+    restemp.innerHTML = 70;
 }
 
-
-function submit(event) {
-    event.preventDefault();
-    let inp = document.querySelector("#formGroupExampleInput");
-    let result = document.querySelector("#city");
-    result.innerHTML = inp.value;
-    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=${city},au&appid=${apiKey}&units=metric";
-    let apiKey = "7e7a6afccc2b04cbebfc65ac42faa6dc";
-    axios.get(apiURL).then(showTemp);
-}
-
-
-let search = document.querySelector("#srch");
-search.addEventListener("click", submit);
+celtemp.addEventListener("click", showcel);
+fahrtemp.addEventListener("click", showfahr);
